062de55b7d93e2f0d7590e960646962a5d3551cb
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2022-11-03 10:25:15 +0000

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="py-8">
+      <h1 className="text-2xl text-center text-white">Next 13 Basics by @codewithguillaume</h1>
+    </header>
+  )
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type Post = {
+  title: string
+  content: string
 }
 
-export default function handler(
+type Data = {
+  posts: Post[]
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const data = await new Promise((resolve) => {
+    setTimeout(resolve, 2000)
+  })
+
+  res.status(200).json({
+    posts: [
+      {
+        title: "This is my new post",
+        content: "This is the content"
+      },
+      {
+        title: "This is another new post",
+        content: "This is the content of this another post"
+      }
+    ]
+  })
 }
